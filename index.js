7b0badc875f7c61ee7270ab0c9c0b58518a2aa6b
JonasFernandes <jonas.fernandes@outlook.com>
2018-08-04 01:16:02 -0300

    download button and effects

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ soundButtons.forEach(el => {
 })
 
 video.play()
+
+const effect = () => {
+  const social = document.querySelector('.social-nav')
+  const media = document.querySelector('.media-channels')
+  const slogan = document.querySelector('.slogan')
+  const download = document.querySelector('.download')
+
+  social.classList.add('enter')
+  media.classList.add('enter')
+  slogan.classList.add('enter')
+  download.classList.add('enter')
+}
+
+setTimeout(() => {
+  effect()
+}, 1000)
